8b7dc7893977c92b76cbfa030ea82845f2dcf979
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2026-03-31 21:57:44 +0600

    banner added

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
-
+import { IoLogoGooglePlaystore } from "react-icons/io5";
+import { FaAppStoreIos } from "react-icons/fa";
 const Banner = () => {
     return (
-        <div className='container h-screen bg-base-200'>
+        <div className='container bg-base-200 mx-auto px-4 py-2'>
             <section>
                 <div className='flex flex-col items-center justify-center'>
                     <h1 className='text-5xl font-bold text-center mt-20'>
                         <span>We Build</span> <br />
                         <span className='text-blue-900'>Productive</span> Apps
                     </h1>
+                    <p className='text-lg text-center mt-4 text-gray-600'>
+                        We create innovative solutions that drive business growth and user satisfaction. Our team of experts is dedicated to delivering <br/> high-quality products that meet your unique needs and exceed your expectations.
+                    </p>
+                    <div className='flex gap-4 mt-6'>
+                        <button className='btn btn-primary py-3'>
+                            <IoLogoGooglePlaystore />
+                            <span className='ml-2'>Google Play</span>
+                        </button>
+                        <button className='btn btn-secondary py-3'>
+                            <FaAppStoreIos />
+                            <span className='ml-2'>App Store</span>
+                        </button>
+                    </div>
+
+                    <div className='mt-10 '>
+                        <img className='image-full' src="/src/assets/hero (1).png" alt="" />
+                    </div>
                 </div>
             </section>
         </div>
